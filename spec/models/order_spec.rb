68b6a2361eb9a9601ea107d560290118d6b7ff36
Andrew Leung <andrew@ualberta.net>
2019-01-18 22:59:51 +0000

    Test: Order is open

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'new orders are created with :open status' do
+    order = Order.new
+
+    order.save
+
+    expect(order.status).to eq('open')
+  end
 end
