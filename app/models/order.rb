fd0c84eb72aa4cdcfdc6ee2c1d9753804cb1dfe1
Andrew Leung <andrew@ualberta.net>
2019-01-19 01:04:55 +0000

    Code: Update status to failed on save failure

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   before_save :modify_status
+  after_rollback :mark_failure
 
   private
 
@@ -7,5 +8,9 @@ class Order < ApplicationRecord
     self.status = 'open'
     self.status = 'pending' if attribute_changed?(:amount)
     self.status = 'received' if !amount.nil? && (amount == received)
+  end
+
+  def mark_failure
+    self.update_column(:status, 'failed')
   end
 end
